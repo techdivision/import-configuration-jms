a84e06a84572a019dbff897c95dcddb423b62bd5
Kenza Yamlahi <k.yamlahi@techdivision.com>
2021-10-21 16:35:53 +0200

    refactoring if statement

--- src/Configuration/Subject.php
+++ src/Configuration/Subject.php
@@ -516,7 +516,7 @@ class Subject implements SubjectConfigurationInterface, ListenerAwareConfigurati
         if ($this->okFileNeeded) {
             // query whether or not a specific file has been
             // specified, if yes, we do NOT need an .OK file
-            if ($this->getConfiguration() instanceof ConfigurationInterface && empty($this->getConfiguration()->getFilename())) {
+            if ($this->getConfiguration() instanceof ConfigurationInterface && !empty($this->getConfiguration()->getFilename())) {
                 return false;
             } else {
                 return true;
